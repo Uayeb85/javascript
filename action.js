555119544c674a3531355dc67a2f4155d310152f
Uayeb85 <gustavo.aa18@gmail.com>
2017-04-23 16:10:10 -0500

    Java1

---
+++ action.js
@@ -0,0 +1,13 @@
+function Hola() {
+  /* La funcion ira aqui*/
+ alert("Hola Mundo");
+}
+
+function multiplicar(){
+  a= document.getElementById('first').value;
+  b= document.getElementById('second').value;
+
+  //alert(a*b);
+document.getElementById('result').innerHTML =a*b;
+
+}
